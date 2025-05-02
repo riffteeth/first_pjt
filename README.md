8ab0460b6cedf8be3304b649bbd2760f101b2725
english3131 <english3131@naver.com>
2025-05-02 10:08:48 +0900

    dddd

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # first_pjt
 first_pjt
+
+
+saved 2025.05.01
+
+d
+d
+d
+d
---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello, GitHub + Python 자동화!")
+print("Hello, GitHub + Python 자동화!")
+
+print("Hello, GitHub + Python 자동화!")
+
+print("Hello, GitHub + Python 자동화!")
+
